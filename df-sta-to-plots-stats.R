f4cc5d075c00ce631f61d8c18c9bfde280d77fb3
IanGMalone <ian.g.malone@gmail.com>
2020-06-24 19:28:42 -0400

    Updates June 24, 2020

--- df-sta-to-plots-stats.R
+++ df-sta-to-plots-stats.R
@@ -38,17 +38,17 @@ percent_change <- function(old_val, new_val) {
 #### load STA data as dataframe
 df <- data.frame(read.csv('C:/Users/iangm/Desktop/df_STA_2020_06_14_clean.csv'))
 df[,'Animal'] = toupper(df[,'Animal'])
-dt_STA <- data.table(df)
+dt_STA = data.table(df)
 
 
 #### make data table for day 1, 2, 3, 4
 #### remove stim artifact (samples 0:50) <---- maybe remove less than 50 (50 samples = 2.5ms)
 #### only keep left side EMG in data table
-dt_STA <- subset(dt_STA, Day %in% c(1, 2, 3, 4) & Sample %in% c(50:300) & Side %in% c('Left'))
+dt_STA = subset(dt_STA, Day %in% c(1, 2, 3, 4) & Sample %in% c(50:300) & Side %in% c('Left'))
 
 
 #### calculate AUC, dropping the Sample and STA_Amplitude columns
-dt_STA <- dt_STA[, 
+dt_STA = dt_STA[, 
                                .(
                                  AUC = trapz(Sample, STA_Amplitude)
                                ),
@@ -56,7 +56,12 @@ dt_STA <- dt_STA[,
 
 
 #### break Day column into columns for day 1 AUC and day 4 AUC
-dt_STA <- dcast(dt_STA, Animal + Side + Stim_Amplitude ~ Day, value.var = "AUC")
+dt_STA = dcast(dt_STA, Animal + Side + Stim_Amplitude ~ Day, value.var = "AUC")
+
+
+#### new dt made to use 100 uA as baseline (continued below after dt_STA processing)
+keeps <- c(100,200,300,400,500)
+dt_STA_100_baseline = subset(dt_STA, Stim_Amplitude %in% c(100,200,300,400,500))
 
 
 #### rename columns
@@ -74,14 +79,14 @@ dt_STA[, Day4_PercentChange := percent_change(Day1_AUC, Day4_AUC)]
 
 
 #### define animal groups
-injstim <- c("N09", "N10", "N11", "N13")
-injnostim <- c("N14", "N15", "N16", "N21", "N22", "N23")
-noinjstim <- c("N01", "N04", "N05")
-noinjnostim <- c("N17", "N19", "N20", "N24", "N25", "N26")
-# injcdrstim <- c("CD01", "CD02")
-# injtrkbstim <- c("T01")
-# injnoemgnostim <- c("EZ01", "EZ02")
-# noinjnoemgnostim <- c("EZ03", "EZ04")
+injstim = c("N09", "N10", "N11", "N13")
+injnostim = c("N14", "N15", "N16", "N21", "N22", "N23")
+noinjstim = c("N01", "N04", "N05")
+noinjnostim = c("N17", "N19", "N20", "N24", "N25", "N26")
+# injcdrstim = c("CD01", "CD02")
+# injtrkbstim = c("T01")
+# injnoemgnostim = c("EZ01", "EZ02")
+# noinjnoemgnostim = c("EZ03", "EZ04")
 
 
 #### add animal group column
@@ -95,16 +100,13 @@ dt_STA[Animal %in% noinjnostim, Group := "No Injury + No Stimulation, n=6"]
 # dt_STA[Animal %in% noinjnoemgnostim, Group := "No Injury + No EMG + No Stimulation"]
 
 
-#--------------------------------- make new dt, dt_STA_long with columns Day, AUC, Percent_Change
-
-
 #### melt
-dt_STA_testt = melt(dt_STA, id = c("Animal", "Side", "Stim_Amplitude", "Group"))
-dt_STA_testt[, c("Day", "Measure") := tstrsplit(variable, "_", fixed = TRUE)]
-dt_STA_testt[,variable:=NULL]
-dt_STA_testt <- dcast(dt_STA_testt, Animal + Side + Stim_Amplitude + Group + Day ~ Measure, value.var = "value")
-setnames(dt_STA_testt, "PercentChange", "Percent_Change")
-setcolorder(dt_STA_testt, c("Animal", "Side", "Stim_Amplitude", "Day", "AUC", "Percent_Change", "Group"))
+dt_STA = melt(dt_STA, id = c("Animal", "Side", "Stim_Amplitude", "Group"))
+dt_STA[, c("Day", "Measure") := tstrsplit(variable, "_", fixed = TRUE)]
+dt_STA[,variable:=NULL]
+dt_STA = dcast(dt_STA, Animal + Side + Stim_Amplitude + Group + Day ~ Measure, value.var = "value")
+setnames(dt_STA, "PercentChange", "PercentChangeAUC_from_Day1")
+setcolorder(dt_STA, c("Animal", "Side", "Stim_Amplitude", "Day", "AUC", "Percent_Change", "Group"))
 
 
 #### save dt to a .csv
@@ -112,7 +114,54 @@ setcolorder(dt_STA_testt, c("Animal", "Side", "Stim_Amplitude", "Day", "AUC", "P
 
 
 
+
+#### continue with processing other dt for 100 uA baseline
+dt_STA_100_500 = melt(dt_STA_100_500, id = c("Animal", "Side", "Stim_Amplitude"))
+dt_STA_100_500 = dcast(dt_STA_100_500, Animal + Side + variable ~ Stim_Amplitude, value.var = "value")
+
+
+#### rename columns
+setnames(dt_STA_100_500, c('100','200','300','400','500','variable'), c('s100','s200','s300','s400','s500','Day'))
+
+
+#### calculate percent change from 100 uA to other stim amps
+dt_STA_100_500[, pc100 := percent_change(s100, s100)]
+dt_STA_100_500[, pc200 := percent_change(s100, s200)]
+dt_STA_100_500[, pc300 := percent_change(s100, s300)]
+dt_STA_100_500[, pc400 := percent_change(s100, s400)]
+dt_STA_100_500[, pc500 := percent_change(s100, s500)]
+
+#### drop old columns
+dt_STA_100_500[, c('s100','s200','s300','s400','s500'):=NULL]  # remove two columns
+
+
+#### add animal group column
+dt_STA_100_500[Animal %in% injstim, Group := "Injury + Stimulation, n=4"]
+dt_STA_100_500[Animal %in% injnostim, Group := "Injury + No Stimulation, n=6"]
+dt_STA_100_500[Animal %in% noinjstim, Group := "No Injury + Stimulation, n=3"]
+dt_STA_100_500[Animal %in% noinjnostim, Group := "No Injury + No Stimulation, n=6"]
+
+
+# define columns to compute mean of
+cols <- c("pc400")
+# compute means for selected columns and rename the output
+dt_test <- dt_STA_100_500[, lapply(.SD, mean), .SDcols = cols, by = .(Day, Group)
+              ][, setnames(.SD, cols, paste(cols, "mean", sep = "_"))]
+
+
+
+
+
+
+
+
 #### ---- plotting ---- ####
+
+ggplot(dt_STA_100_500, aes(x=as.factor(Group), y=pc400, fill=Measure)) +
+  geom_bar(position=position_dodge(), stat="identity", colour='black') +
+  geom_errorbar(aes(ymin=mean-sd, ymax=mean+sd), width=.2,position=position_dodge(.9))
+
+
 
 #### in progress plots
 #WHERE IS N11 ?????????
