14db618e9bd70cb93c2c0af5878ee23882ba7211
IanGMalone <ian.g.malone@gmail.com>
2020-06-23 22:56:00 -0400

    Need to do some DSP (anti-alias, upsample, downsample)

--- mep-to-sta-batch.py
+++ mep-to-sta-batch.py
@@ -157,6 +157,49 @@ df_MEP.to_csv(r'C:\Users\iangm\Desktop\df_MEP_2020_06_14_clean.csv', index = Fal
 df_STA.to_csv(r'C:\Users\iangm\Desktop\df_STA_2020_06_14_clean.csv', index = False)
 
 
+
+
+
+
+
+#### will this work? we need a lowpass anti aliasing filter
+#lowpass at 12500 and at 10000 (maybe go even lower to like 8000)
+#do an fft to see what frequencies are in the signal
+#upsample then downsample so you get downsampling of non integer ration (1.25)
+
+
+def resample(smp, scale=1.0):
+    """Resample a sound to be a different length
+    Sample must be mono.  May take some time for longer sounds
+    sampled at 44100 Hz.
+
+    Keyword arguments:
+    scale - scale factor for length of sound (2.0 means double length)
+    """
+    # f*ing cool, numpy can do this with one command
+    # calculate new length of sample
+    n = round(len(smp) * scale)
+    # use linear interpolation
+    # endpoint keyword means than linspace doesn't go all the way to 1.0
+    # If it did, there are some off-by-one errors
+    # e.g. scale=2.0, [1,2,3] should go to [1,1.5,2,2.5,3,3]
+    # but with endpoint=True, we get [1,1.4,1.8,2.2,2.6,3]
+    # Both are OK, but since resampling will often involve
+    # exact ratios (i.e. for 44100 to 22050 or vice versa)
+    # using endpoint=False gets less noise in the resampled sound
+    return numpy.interp(
+        numpy.linspace(0.0, 1.0, n, endpoint=False), # where to interpret
+        numpy.linspace(0.0, 1.0, len(smp), endpoint=False), # known positions
+        smp, # known data points
+        )
+
+
+
+
+
+
+
+
     
 #### questions ####
 #!!!! important
@@ -164,6 +207,12 @@ df_STA.to_csv(r'C:\Users\iangm\Desktop\df_STA_2020_06_14_clean.csv', index = Fal
 # or do you smooth (moving average), rectify, etc?
 # moving average window latencies
 # split functions so they each do 1 thing
+
+
+
+
+
+
 
 
 #### scrap ####
