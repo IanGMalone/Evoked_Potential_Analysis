16d64453f9f3389bcce62905e2185c16804cffee
IanGMalone <ian.g.malone@gmail.com>
2020-07-07 12:47:51 -0400

    Updates

--- read-smrx.py
+++ read-smrx.py
@@ -12,16 +12,16 @@ ephys data.
 
 import numpy as np
 from scipy import signal
-import pandas as pd
+#import pandas as pd
 import h5py
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set(style='ticks')
-
+from scipy.signal import butter, lfilter, freqz
 
 
 path = 'C:/Users/iangm/Desktop/'
-file_name = 'n16_4_mep.mat'
+file_name = 'n10_1_mep.mat'
 
 
 # load file and extract keys
@@ -62,10 +62,75 @@ peak_heights = stim_peaks[1]['peak_heights']
 mep_time_ms = 12 
 mep_sample_length = round((mep_time_ms/1000)*samp_freq)
 
+#### filtering
+
+
+def butter_lowpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
+
+def butter_lowpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+
+# Filter requirements.
+cut_norm_rad = (np.pi)/5 # desired normalized cutoff freq, rad/s
+order = 10
+fs = 20000       # sample rate, Hz
+cutoff = int(round((cut_norm_rad * fs) / (2*np.pi))) # desired cutoff freq, Hz
+
+# Get the filter coefficients so we can check its frequency response.
+b, a = butter_lowpass(cutoff, fs, order)
+
+# Plot the frequency response.
+w, h = freqz(b, a, worN=8000)
+plt.subplot(2, 1, 1)
+plt.plot(0.5*fs*w/np.pi, np.abs(h), 'b')
+plt.plot(cutoff, 0.5*np.sqrt(2), 'ko')
+plt.axvline(cutoff, color='k')
+plt.xlim(0, 0.5*fs)
+plt.title("Lowpass Filter Frequency Response")
+plt.xlabel('Frequency [Hz]')
+plt.grid()
+
+### my code
+
+y_raw = rEMG[10000:11000]
+y_filt = butter_lowpass_filter(y_raw, cutoff, fs, order)
+
+# =============================================================================
+# 
+# # Demonstrate the use of the filter.
+# # First make some data to be filtered.
+# T = 5.0         # seconds
+# n = int(T * fs) # total number of samples
+# t = np.linspace(0, T, n, endpoint=False)
+# # "Noisy" data.  We want to recover the 1.2 Hz signal from this.
+# data = np.sin(1.2*2*np.pi*t) + 1.5*np.cos(9*2*np.pi*t) + 0.5*np.sin(12.0*2*np.pi*t)
+# 
+# # Filter the data, and plot both the original and filtered signals.
+# y = butter_lowpass_filter(data, cutoff, fs, order)
+# =============================================================================
+
+plt.subplot(2, 1, 2)
+plt.plot(range(10000,11000), y_raw, 'b-', label='data')
+plt.plot(range(10000,11000), y_filt, 'g-', linewidth=2, label='filtered data')
+plt.xlabel('Time [sec]')
+plt.grid()
+plt.legend()
+
+plt.subplots_adjust(hspace=0.35)
+plt.show()
 
 
 
+y_res = signal.upfirdn(fir_coeffs, y_raw, up=5, down=4, mode='smooth')
 
+# shift signal to compensate for delay
 
 
 
