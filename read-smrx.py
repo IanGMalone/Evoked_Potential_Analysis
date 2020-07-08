5b76690b09078b7fa3f4e3f2634649fcea4c703c
IanGMalone <ian.g.malone@gmail.com>
2020-07-08 14:52:23 -0400

    Added FIR filter

--- read-smrx.py
+++ read-smrx.py
@@ -17,7 +17,6 @@ import h5py
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set(style='ticks')
-from scipy.signal import butter, lfilter, freqz
 
 
 path = 'C:/Users/iangm/Desktop/'
@@ -62,97 +61,50 @@ peak_heights = stim_peaks[1]['peak_heights']
 mep_time_ms = 12 
 mep_sample_length = round((mep_time_ms/1000)*samp_freq)
 
-#### filtering
 
+#Plot frequency and phase response
+def mfreqz(b,a=1):
+    w,h = signal.freqz(b,a)
+    h_dB = 20 * np.log10 (abs(h))
+    plt.subplot(211)
+    plt.plot(w/max(w),h_dB)
+    plt.ylim(-150, 5)
+    plt.ylabel('Magnitude (db)')
+    plt.xlabel(r'Normalized Frequency (x$\pi$rad/sample)')
+    plt.title(r'Frequency response')
+    plt.subplot(212)
+    h_Phase = np.unwrap(np.arctan2(np.imag(h),np.real(h)))
+    plt.plot(w/max(w),h_Phase)
+    plt.ylabel('Phase (radians)')
+    plt.xlabel(r'Normalized Frequency (x$\pi$rad/sample)')
+    plt.title(r'Phase response')
+    plt.subplots_adjust(hspace=0.5)
 
-def butter_lowpass(cutoff, fs, order=5):
-    nyq = 0.5 * fs
-    normal_cutoff = cutoff / nyq
-    b, a = butter(order, normal_cutoff, btype='low', analog=False)
-    return b, a
-
-def butter_lowpass_filter(data, cutoff, fs, order=5):
-    b, a = butter_lowpass(cutoff, fs, order=order)
-    y = lfilter(b, a, data)
-    return y
-
-
-# Filter requirements.
-cut_norm_rad = (np.pi)/5 # desired normalized cutoff freq, rad/s
-order = 10
-fs = 20000       # sample rate, Hz
-cutoff = int(round((cut_norm_rad * fs) / (2*np.pi))) # desired cutoff freq, Hz
-
-# Get the filter coefficients so we can check its frequency response.
-b, a = butter_lowpass(cutoff, fs, order)
-
-# Plot the frequency response.
-w, h = freqz(b, a, worN=8000)
-plt.subplot(2, 1, 1)
-plt.plot(0.5*fs*w/np.pi, np.abs(h), 'b')
-plt.plot(cutoff, 0.5*np.sqrt(2), 'ko')
-plt.axvline(cutoff, color='k')
-plt.xlim(0, 0.5*fs)
-plt.title("Lowpass Filter Frequency Response")
-plt.xlabel('Frequency [Hz]')
-plt.grid()
-
-### my code
+#Plot step and impulse response
+def impz(b,a=1):
+    l = len(b)
+    impulse = np.repeat(0.,l); impulse[0] =1.
+    x = np.arange(0,l)
+    response = signal.lfilter(b,a,impulse)
+    plt.subplot(211)
+    plt.stem(x, response)
+    plt.ylabel('Amplitude')
+    plt.xlabel(r'n (samples)')
+    plt.title(r'Impulse response')
+    plt.subplot(212)
+    step = np.cumsum(response)
+    plt.stem(x, step)
+    plt.ylabel('Amplitude')
+    plt.xlabel(r'n (samples)')
+    plt.title(r'Step response')
+    plt.subplots_adjust(hspace=0.5)
+    
+n = 61 # delay of 30 samples
+fir_coeffs = signal.firwin(n, cutoff = (np.pi/5), window = "hamming")
+#Frequency and phase response
+mfreqz(fir_coeffs)
 
 y_raw = rEMG[10000:11000]
-y_filt = butter_lowpass_filter(y_raw, cutoff, fs, order)
-
-# =============================================================================
-# 
-# # Demonstrate the use of the filter.
-# # First make some data to be filtered.
-# T = 5.0         # seconds
-# n = int(T * fs) # total number of samples
-# t = np.linspace(0, T, n, endpoint=False)
-# # "Noisy" data.  We want to recover the 1.2 Hz signal from this.
-# data = np.sin(1.2*2*np.pi*t) + 1.5*np.cos(9*2*np.pi*t) + 0.5*np.sin(12.0*2*np.pi*t)
-# 
-# # Filter the data, and plot both the original and filtered signals.
-# y = butter_lowpass_filter(data, cutoff, fs, order)
-# =============================================================================
-
-plt.subplot(2, 1, 2)
-plt.plot(range(10000,11000), y_raw, 'b-', label='data')
-plt.plot(range(10000,11000), y_filt, 'g-', linewidth=2, label='filtered data')
-plt.xlabel('Time [sec]')
-plt.grid()
-plt.legend()
-
-plt.subplots_adjust(hspace=0.35)
-plt.show()
-
-
-
 y_res = signal.upfirdn(fir_coeffs, y_raw, up=5, down=4, mode='smooth')
 
-# shift signal to compensate for delay
 
-
-
-
-
-# =============================================================================
-#     
-# sns.lineplot(x=time[0:500000], y=lEMG[0:500000])
-# plt.show()
-# 
-# length=500000
-# data = np.array([time[0:length], lEMG[0:length], rEMG[0:length]])
-# data = np.transpose(data)
-# df = pd.DataFrame(data=data, index=np.arange(length), columns=["Time", "Left Diaphragm EMG", "Right Diaphragm EMG"])
-# 
-# meltdf = pd.melt(df, id_vars=['Time'], value_vars=['Left Diaphragm EMG', 'Right Diaphragm EMG'])
-# meltdf.rename(columns={'variable':'side', 'value':'EMG Amplitude (V)'}, inplace=True)
-# 
-# 
-# g = sns.FacetGrid(meltdf, row="side", height=1.7, aspect=4,)
-# g.map(sns.lineplot, 'Time', 'EMG Amplitude (V)');
-# 
-# 
-# meltdf.to_csv(r'C:\Users\iangm\Desktop\dffff.csv', index = False)
-# =============================================================================
