702020819cb98425f58474dab96c44ce3eae3b5e
unknown <ian.g.malone@gmail.com>
2020-12-07 19:28:41 -0500

    find_animal and find_day functions

--- mep-to-sta-batch.py
+++ mep-to-sta-batch.py
@@ -35,12 +35,10 @@ import time
 #### define functions
 def file_to_df(path, file_name, df, low_amp_list, mep_time_ms, col_names=['Animal', 'Day', 'Side', 'Stim_Amplitude', 'Sample', 'EMG_Amplitude']):
     '''Takes .mat file containing MEP data and returns a dataframe of that data'''
-
     # load file and extract keys
     filepath = path + file_name
     raw_data = h5py.File(filepath)
     all_keys = list(raw_data.keys())
-    
     # define variables of interest
     leftEMG_vars = ['LDia', 'LDIA', 'lEMG_raw']
     rightEMG_vars = ['RDia', 'RDIA', 'rEMG_raw']
@@ -49,32 +47,27 @@ def file_to_df(path, file_name, df, low_amp_list, mep_time_ms, col_names=['Anima
     leftEMG = [key for var in leftEMG_vars for key in all_keys if var in key][0]
     rightEMG = [key for var in rightEMG_vars for key in all_keys if var in key][0]
     stim_wave = [key for var in stim_vars for key in all_keys if var in key][0]
-
     # unpack variables from .mat file
-    animal = file_name.split('_')[3]
-    day = file_name.split('_')[4].split('.')[0]
+    animal = find_animal(file_name)
+    day = find_day(file_name)
     stim = raw_data[stim_wave]['values'][0]
     samp_freq = int(round(1/(raw_data[stim_wave]['interval'][0][0])))
     rEMG = raw_data[rightEMG]['values'][0]
     lEMG = raw_data[leftEMG]['values'][0]
-    
-    # # below is for non-hd5 files
+    ### below is for non-hd5 files
     # stim = stim[0][0].flatten()
     # samp_freq = 1/float(samp_freq[0][0].flatten())
     # rEMG = rEMG[0][0].flatten()
     # lEMG = lEMG[0][0].flatten()
-     
     # find location of stimulation pulses (sample number)
     #!!!!!!! VERIFY THIS SECTION
     stim_peaks = signal.find_peaks(stim, height=0.09, distance=6)
     peak_locs = stim_peaks[0]
     peak_heights = stim_peaks[1]['peak_heights']
-    
     # get chunks of meps and make dataframe
     df_mep = pd.DataFrame(columns=col_names)
     #!!!!! make ms an argument?
     # Animals <= N13 were sampled at 20 kHz, animals > N13 were at 25 kHz
-
     mep_sample_length = round((mep_time_ms/1000)*samp_freq)
 
     for i in np.arange(len(peak_locs)):
@@ -90,24 +83,17 @@ def file_to_df(path, file_name, df, low_amp_list, mep_time_ms, col_names=['Anima
     return df_mep
     
 
-
 def list_files(rootdir, extension='.mat'):
     '''Return list of all files with specific extension in given directory (subfolders included)'''
-    
     list_of_files=[]
-    
     for subdir, dirs, files in os.walk(rootdir):
         for file in files:
             if file.endswith(extension):
                 list_of_files.append(file)
-
     return list_of_files
-
-
 
 def mep_to_df(animal, day, side, amp, mep, samp_freq, colnames=['Animal', 'Day', 'Side', 'Stim_Amplitude', 'Sample', 'EMG_Amplitude']):
     '''Make data frame given various MEP information'''
-    
     #the line below downsamples files to 5 kHz sampling frequency
     mep_downsamp = signal.decimate(mep, int(samp_freq/5000))
     animal_array = np.repeat(animal, len(mep_downsamp))
@@ -115,21 +101,28 @@ def mep_to_df(animal, day, side, amp, mep, samp_freq, colnames=['Animal', 'Day',
     side_array = np.repeat(side, len(mep_downsamp))
     amp_array = np.repeat(amp, len(mep_downsamp))
     samples = np.arange(len(mep_downsamp))
-    
     d = {colnames[0]:animal_array, colnames[1]:day_array, colnames[2]:side_array, colnames[3]:amp_array, colnames[4]:samples, colnames[5]:mep_downsamp}
     df = pd.DataFrame(d, columns=colnames)
-    
     return df
-
-
 
 def round_to_5(number):
     '''Round an input number to the nearest multiple of 5'''
-    
     num_out = round(number/5)*5
-    
     return num_out
 
+def find_day(filename):
+    '''Find day given filename (current structure of names)'''
+    if filename.split('_')[0] == '2020':
+        return filename.split('_')[4].split(".")[0]
+    else:
+        return 'D0' + filename.split('_')[1]
+        
+def find_animal(filename):
+    '''Find animal given filename (current structure of names)'''
+    if filename.split('_')[0] == '2020':
+        return filename.split('_')[3]
+    else:
+        return filename.split('_')[0].upper()
 
 
 
