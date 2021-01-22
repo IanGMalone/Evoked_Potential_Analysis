b3c759ce0ce63d49c3f1036a71097923274df96a
unknown <ian.g.malone@gmail.com>
2021-01-22 16:12:00 -0500

    Updates

--- mep-to-sta-batch.py
+++ mep-to-sta-batch.py
@@ -141,12 +141,13 @@ def find_animal(filename):
 
 
 
+proj = 'SPARC'
 
 #### do processing
 startTime = datetime.now()
 
 # specify locations and files and make empty dataframe
-rootdir = 'D:\\TOTAL_MEP_MAT_NEIL\\'
+rootdir = 'D:\\TOTAL_MEP_MAT_' + proj + '\\'
 cols = ['Animal', 'Day_Postop', 'Day_Stim', 'Side', 'Stim_Amplitude', 'Sample', 'EMG_Amplitude']
 df_MEP = pd.DataFrame(columns=cols)
 high_v_list = ['N01', 'N04', 'N05', 'N09', 'N10', 'N11', 'N13' ]
@@ -169,8 +170,8 @@ df_STA.rename(columns={'EMG_Amplitude': 'STA_Amplitude'}, inplace=True)
 
 # save dataframes to CSV files
 date_str = time.strftime("%Y_%m_%d")
-df_MEP.to_csv(r'D:\Data\\df_MEP_NEIL_' + date_str + '.csv', index = False)
-df_STA.to_csv(r'D:\\Data\\df_STA_NEIL_' + date_str + '.csv', index = False)
+df_MEP.to_csv(r'D:\Data\\df_MEP_' + proj +'_' + date_str + '.csv', index = False)
+df_STA.to_csv(r'D:\\Data\\df_STA_' + proj +'_' + date_str + '.csv', index = False)
 
 
 
