458d6280bad7ffc360bf97d5816ecefb9c887c08
unknown <ian.g.malone@gmail.com>
2021-02-18 10:02:28 -0500

    Updates

--- statistics.R
+++ statistics.R
@@ -1,2 +1,17 @@
 library(lme4)
+library(psycho)
+library(tidyverse)
+
 df_p2p_scaled <- read.csv(file = 'D:\\df_p2p_scaled.csv')
+
+
+fit <- lmer(p2p_amplitude_scaled ~ Stim_Amplitude*Day_Stim + (Day_Stim|Group) + (1|Animal), data=df_p2p_scaled)
+summary(fit)
+
+results <- analyze(fit, CI = 95)
+
+summary(results) %>% 
+  mutate(p = psycho::format_p(p))
+
+# random intercept ... lmer(y ~ x + (1|randomGroup), data = myData)
+# random slope ... lmer(y ~ x + (randomSlope|randomGroup), data = myData)
